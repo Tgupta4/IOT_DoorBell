d3b9efbfda382c824d76c3e3b7afe1f352a189a9
tanishtg <tanishgupta2999@gmail.com>
2024-01-09 12:23:53 -0500

    File update

---
+++ scripts/credentials.py
@@ -0,0 +1,15 @@
+cloudinary = {
+    "key": "KEY",
+    "secret": "SECRET_KEY",
+    "env": "ENVIRNMENT",
+    "name": "NAME"
+}
+
+ifttt = {
+    "url": "URL_HERE",
+
+    "values": {
+        "value1": "PHONE_NUMBER",
+        "value2": "MESSAGE_TO_SEND"
+    }
+}
