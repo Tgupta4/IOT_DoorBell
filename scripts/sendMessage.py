2f73ce025ae90e9fb376ad4167c7e2bb80844a2e
tanishtg <tanishgupta2999@gmail.com>
2024-01-11 14:21:44 -0500

    Update

--- scripts/sendMessage.py
+++ scripts/sendMessage.py
@@ -12,23 +12,9 @@ def sendSMS(msg=False, phone=False):
     values['value2'] = msg or "Np Message given"
     if(phone):
         values['value1'] = phone
-    # x = //requests.post(url, values)
-    # url = url + "?" + urllib.parse.urlencode(values)
-    # //value1="+values['value1']+"&value2="+values['value2']
     x=requests.get(url + "?" + urllib.parse.urlencode(values))
     # print(values, x.text)
     print("start")
     print(values)
     print(x.text)
     print("end")
-
-
-
-
-    # def sendSMS(msg=False, phone=False):
-    # print("send sms")
-    # values['value2'] = msg or "Np Message given"
-    # if(phone):
-    #     values['value1'] = phone
-    # x = requests.post(url, values)
-    # print(values, x.text)
